fe20d42fa6966a5f0a90bcdbf284baa048324be3
山下竜二 <r.yamashita@mobius-box.biz>
2026-03-30 22:04:18 +0900

    Initial Commit

---
+++ iOS-Sample-MVVMUITests/iOS_Sample_MVVMUITests.swift
@@ -0,0 +1,41 @@
+//
+//  iOS_Sample_MVVMUITests.swift
+//  iOS-Sample-MVVMUITests
+//
+//  Created by 山下竜二 on 2026/03/30.
+//
+
+import XCTest
+
+final class iOS_Sample_MVVMUITests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+
+        // In UI tests it is usually best to stop immediately when a failure occurs.
+        continueAfterFailure = false
+
+        // In UI tests it’s important to set the initial state - such as interface orientation - required for your tests before they run. The setUp method is a good place to do this.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    @MainActor
+    func testExample() throws {
+        // UI tests must launch the application that they test.
+        let app = XCUIApplication()
+        app.launch()
+
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    @MainActor
+    func testLaunchPerformance() throws {
+        // This measures how long it takes to launch your application.
+        measure(metrics: [XCTApplicationLaunchMetric()]) {
+            XCUIApplication().launch()
+        }
+    }
+}
